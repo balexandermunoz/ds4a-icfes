7cb255632f3ea4aa85c478617660ee53b5f62c87
Brayan Muñoz <balexander.munoz@udea.edu.co>
2022-07-04 22:53:39 -0500

    :alien: API logic updated.

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -10,8 +10,7 @@ import Plots from './Plots';
 import '../assets/styles/Analysis.css';
 
 const Analysis = () => {
-    // Let's write the logic to select buttons:
-    const { data:colegiosData, isLoading, isError } = useFetch('https://ec2-54-83-150-132.compute-1.amazonaws.com/MasterData/School');
+    const { data:colegiosData, isLoading, isError } = useFetch('https://icfes-ds4a.xyz/MasterData/School');
 
     const [dep, setDep] = useState("");
     const [mun, setMun] = useState("");
@@ -44,7 +43,7 @@ const Analysis = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}>
-            <h1>How is my school doing in the ICFES test?</h1>
+            <h1>How is my school doing in the SABER test?</h1>
             {isLoading && <div className='analysis--spinner'> Getting data... <Spinner name="circle" color={"steelblue"} /> </div>}
             {isError && <div>Sorry, data not aviliable <span>&#128532;</span> </div>}
             {colegiosData && <div className={"analysis--buttons"}>
@@ -54,7 +53,7 @@ const Analysis = () => {
             </div>}
             {Boolean(col) && <Plots cole={col} />}
             <div className={"analysis--link"}>
-                Do you want to know how to increase your ICFES score? See our model here:
+                Do you want to know how to increase your SABER score? See our model here:
                 <Link className="analysis--link--button" to="/models"> Go to model! </Link>
             </div>
         </motion.div>);
