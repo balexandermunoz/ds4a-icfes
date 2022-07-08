24452c29a762e6b59f10b81772adbf41aea3754f
Brayan Muñoz <balexander.munoz@udea.edu.co>
2022-07-07 19:14:17 -0500

    :lipstick: Formatted file.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import '../assets/styles/Header.css';
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { ReactComponent as HomeIcon } from "../assets/images/home.svg";
 import { ReactComponent as ScatterIcon } from "../assets/images/scatter-plot.svg";
 
 const Header = () => {
-    const title = "Education analysis Colombia"
+    const title = "Education analysis Colombia";
     return (
         <div className="header">
             <h1 className="header__title">{title}</h1>
             <div className="header__links">
                 <div className={"header__links__button"}>
                     <HomeIcon className={"header__links__icon"} />
-                    <NavLink className="header__links__link" to="/"> Home</NavLink>
+                    <NavLink className="header__links__link" to="/" > Home</NavLink>
                 </div>
                 <div className={"header__links__button"}>
                     <ScatterIcon className={"header__links__icon"} />
@@ -19,11 +19,11 @@ const Header = () => {
                 </div>
                 <div className={"header__links__button"}>
                     <img className={"header__links__icon"} src={require("../assets/images/model.png")} alt="Group icon" />
-                    <NavLink className="header__links__link" to="/models">Models</NavLink>
+                    <NavLink className="header__links__link" to="/models">Model</NavLink>
                 </div>
                 <div className={"header__links__button"}>
                     <img className={"header__links__icon"} src={require("../assets/images/group.png")} alt="Group icon" />
-                    <NavLink className="header__links__link" to="/about">About</NavLink>
+                    <Link className="header__links__link" to="/about">About</Link>
                 </div>
             </div>
         </div>
