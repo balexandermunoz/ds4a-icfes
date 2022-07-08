1186e7fa3d63dd536e2965eb3b62de9df94f072a
Brayan Muñoz <balexander.munoz@udea.edu.co>
2022-07-07 19:16:37 -0500

    :lipstick: Formatted file.

--- src/scripts/useFetch.js
+++ src/scripts/useFetch.js
@@ -5,23 +5,23 @@ const useFetch = (url) => {
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(null);
     useEffect(() => {
-        console.log("Fetching data...")
-        fetch(url, {mode: 'cors'})
-        .then(response => {
-            if(!response.ok) {
-                throw Error('Could not fetch the data for that resource')
-            }
-            return response.json()
-        })
-        .then(data => {
-            setData(data);
-            setIsLoading(false);
-            setIsError(null);
-        })
-        .catch( err => {
-            setIsLoading(false);
-            setIsError(err.message)
-        })
+        console.log("Fetching school data...")
+        fetch(url, { mode: 'cors' })
+            .then(response => {
+                if (!response.ok) {
+                    throw Error('Could not fetch the data for that resource')
+                }
+                return response.json()
+            })
+            .then(data => {
+                setData(data);
+                setIsLoading(false);
+                setIsError(null);
+            })
+            .catch(err => {
+                setIsLoading(false);
+                setIsError(err.message)
+            })
     }, [url]);
 
     return { data, isLoading, isError }
